5349b7f5e2520105925f2244af2f73965cd3b58c
JackMorris1234 <163089764+JackMorris1234@users.noreply.github.com>
2026-09-03 17:14:06 -0400

    Exercises 1.3-1.5
    Completed exercises 1.3-1.5 in the courseinfo assignment

--- Part 1/courseinfo/src/Part.jsx
+++ Part 1/courseinfo/src/Part.jsx
@@ -6,7 +6,7 @@ const Part= (props) => {
 
     return(
         <div>
-            <p>{props.part} {props.exercise}</p>
+            <p>{props.part.name} {props.part.exercises}</p>
         </div>
     )
 }
