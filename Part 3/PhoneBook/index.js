b5df55b5f9e3342e4ad7835c15cbc79ca658f37e
JackMorris1234 <163089764+JackMorris1234@users.noreply.github.com>
2026-09-21 19:13:13 -0400

    3.1-3.8
    Completed exercises 3.1-3.8

---
+++ Part 3/PhoneBook/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const morgan=require('morgan')
+const app = express()
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length]- :response-time ms'))
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    { 
+      "id": "5",
+      "name": "May Poppendieck", 
+      "number": "38-23-6423122"
+    }
+]
+
+
+app.get('/info', (request, response) => {
+  const dateTime=new Date()
+  response.send(`<p>PhoneBook has info for ${persons.length} people</p> <p>${dateTime}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const generateID=()=>{
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body=request.body
+    
+    if (!body.name || !body.number){
+        return response.status(400).json({error:`content missing`})
+    }
+    if(persons.find(person=>body.name==person.name)){
+        return response.status(400).json({error:`Name Already Entered`})
+    }
+    const person={
+        name: body.name,
+        number: body.number,
+        id: generateID()
+    }
+    persons=persons.concat(person)
+    
+    response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id=request.params.id
+    const person=persons.find(person=>person.id===id)
+    if(person){
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+    
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id=request.params.id
+    persons=persons.filter(person=> person.id !== id)
+    response.status(204).end()
+    
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
